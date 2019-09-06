45044516b03d788fbcedab36d0e1088a3c5300d9
hinet <63603636@qq.com>
2019-09-06 14:03:48 +0800

    规范请求返回格式

--- src/Gateways/AliyunGateway.php
+++ src/Gateways/AliyunGateway.php
@@ -15,9 +15,10 @@ use AlibabaCloud\Client\Exception\ServerException;
 class AliyunGateway extends Gateway
 {
     protected $headers;
-    public function send($mobile, $content='',$type='register')
+
+    public function send($mobile, $content = '', $type = 'register')
     {
-        $this->setVerifyCode($mobile,$type);
+        $this->setVerifyCode($mobile, $type);
         //实例化SDK
         AlibabaCloud::accessKeyClient($this->config['app_key'], $this->config['app_secret'])
             ->regionId($this->config['end_point']) // replace regionId as you need
@@ -27,9 +28,11 @@ class AliyunGateway extends Gateway
             $params = [
                 'code' => $this->code
             ];
-            if(is_array($content)){
-                $params = array_merge($content,$params);
+            if (is_array($content)) {
+                $params = array_merge($content, $params);
             }
+
+
             $result = AlibabaCloud::rpc()
                 ->product('Dysmsapi')
                 // ->scheme('https') // https | http
@@ -39,36 +42,34 @@ class AliyunGateway extends Gateway
                 ->host('dysmsapi.aliyuncs.com')
                 ->options([
                     'query' => [
-                        'RegionId' => $this->config['end_point'],
-                        'PhoneNumbers' => $mobile,
-                        'SignName' => $this->config['sign_name'],
-                        'TemplateCode' => $this->getTemplateId($type),
-                        'TemplateParam' => json_encode($params),
+                        'RegionId'        => $this->config['end_point'],
+                        'PhoneNumbers'    => $mobile,
+                        'SignName'        => $this->config['sign_name'],
+                        'TemplateCode'    => $this->getTemplateId($type),
+                        'TemplateParam'   => json_encode($params),
                         'SmsUpExtendCode' => $this->config['sms_up_extend_code'],
                     ],
                 ])
                 ->request();
-            return $result;
+            return $this->response($result->toArray());
         } catch (ClientException $e) {
             echo $e->getErrorMessage() . PHP_EOL;
         } catch (ServerException $e) {
             echo $e->getErrorMessage() . PHP_EOL;
         }
-        return false;
+        return $this->response();
     }
-    public function response($response)
+
+    public function response($response = '')
     {
-        if($response)
-        {
-            $result = json_decode($response, true);
-            if($result['Code'] == 'OK')
-            {
-                return json_encode(['status'=>1,'message'=>'短信发送成功']);
-            }else{
-                return json_encode(['status'=>0,'message'=>'短信发送失败：'.$result['Message'].',错误码：'.$result['Code']]);
+        if (is_array($response)) {
+            if ($response['Code'] == 'OK') {
+                return json_encode(['status' => 1, 'message' => '短信发送成功']);
+            } else {
+                return json_encode(['status' => 0, 'message' => '短信发送失败：' . $response['Message'] . ',错误码：' . $response['Code']]);
             }
-        }else{
-            return json_encode(['status'=>0,'message'=>'Http请求错误']);
+        } else {
+            return json_encode(['status' => 0, 'message' => 'Http请求错误']);
         }
     }
 }
